3e56a6ca600c824b5c4893efeb24663574a26756
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2023-08-31 18:29:37 +0200

    Added debug run script

---
+++ VICOSDemoRunDebug.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+# set VICOS_CUBE_GUI_DIR to the directory of this script
+export VICOS_CUBE_GUI_DIR="$(dirname "$(readlink -f "$0")")"
+export VICOS_CUBE_VIRTENV=/home/vicosdemo/demo
+
+source $VICOS_CUBE_VIRTENV/bin/activate
+
+# start DEMO using ignition
+python -m ignition $VICOS_CUBE_GUI_DIR/launch_demo_debug.yaml
+
+# Clean up: stop and then remove any unclosed containers
+echo ""
+echo "Cleaning up any remaining containers"
+docker stop $(docker ps -q) &> /dev/null
+docker rm $(docker ps -a -q) &> /dev/null
+echo "done"
