3bebae6f9308588cfb798adb4fe230d53d0e0157
Andraž Andolšek Peklaj <andraz.ap@pm.me>
2025-06-02 14:30:54 +0200

    Changes for parallel running of demo programs in background using docker pause/unpause

--- docker_manager.py
+++ docker_manager.py
@@ -24,6 +24,10 @@ class DockerManager():
         self.stop        = False
 
         self.pyecho_loop.wait(10)
+        
+        self.running_containers = dict()
+
+        self.docker_ready      = echolib.Publisher(self.pyecho_client, "containerReady", "int")
 
     def process(self):
 
@@ -43,31 +47,68 @@ class DockerManager():
                 
                 if command[0] == "1":   # Run container
                     print("Run container with id {}".format(command[1]))
-
-                    for image in self.docker.images.list():
-                        
-                        if len(image.tags) > 0 and image.tags[0] == command[1]:
-                            print("Image with matching tag found...")
-
-                            flag = self.__handle_container(command[1])
-
-                            output_channel = "outContainer" + str(command[0])
-                            input_channel  = "inContainer"  + str(command[0])
-
-                            print(f"{output_channel} {input_channel}")
-
-                            w = echolib.MessageWriter()
-                            w.writeString(output_channel + " " + input_channel)
-                            self.pyecho_docker_out.send(w)
+                    
+                    if command[1] in self.running_containers:
+                        print(f"Container {command[1]} already running ... will unpause it.")
+                        # container should already be running - just unpause it
+                        if self.running_containers[command[1]] is not None:
+                            try:
+                                self.running_containers[command[1]].unpause()
+                                print(f"Container {command[1]} unpaused.")
                             
-                            if flag:
+                                # time.sleep(1)
+                                
+                                # manually send signal for container ready since container is already running
+                                for i in range(0,10):
+                                    self.pyecho_loop.wait(10)
+                        
+                                    writer = echolib.MessageWriter()
+                                    writer.writeInt(1)
+                                    self.docker_ready.send(writer)
+                                    print(f"Sending docker_ready from docker manager.")
 
+                                start_fresh_container = False
+        
                                 self.active_container[0] = command[1]
-                                self.active_container[1] = self.docker.containers.run(image.id,\
-                                    device_requests=[docker.types.DeviceRequest(count=1, driver="nvidia", capabilities=[['gpu']])],\
-                                    remove=True, detach=True,\
-                                    volumes = {"/tmp/echo.sock" : {"bind" : "/tmp/echo.sock", "mode" : "rw"}},\
-                                    log_config = docker.types.LogConfig(type=docker.types.LogConfig.types.JSON), environment=dict(PYTHONUNBUFFERED=1))
+                                self.active_container[1] = self.running_containers[command[1]]
+
+                            except:
+                                print("Error unpausing docker container... (removing and starting new)")
+                                self.stop_active_container(do_pause=False)
+                                start_fresh_container = True 
+                           
+                    else:
+                        start_fresh_container = True
+                    
+                    if start_fresh_container:                        
+                        for image in self.docker.images.list():
+                            
+                            if len(image.tags) > 0 and image.tags[0] == command[1]:
+                                print("Image with matching tag found...")
+
+                                flag = self.__handle_container(command[1])
+
+                                output_channel = "outContainer" + str(command[0])
+                                input_channel  = "inContainer"  + str(command[0])
+
+                                print(f"{output_channel} {input_channel}")
+
+                                w = echolib.MessageWriter()
+                                w.writeString(output_channel + " " + input_channel)
+                                self.pyecho_docker_out.send(w)
+                                
+                                if flag:
+                                    print("setting self.active_container to ", command[1])
+                                    self.active_container[0] = command[1]
+                                    self.active_container[1] = self.docker.containers.run(image.id,\
+                                        device_requests=[docker.types.DeviceRequest(count=1, driver="nvidia", capabilities=[['gpu']])],\
+                                        remove=False, detach=True,\
+                                        volumes = {"/tmp/echo.sock" : {"bind" : "/tmp/echo.sock", "mode" : "rw"}},\
+                                        log_config = docker.types.LogConfig(type=docker.types.LogConfig.types.JSON), 
+                                        environment=dict(PYTHONUNBUFFERED=1),
+                                        network_mode="host")           
+
+                                    self.running_containers[command[1]] = self.active_container[1]
 
 
                 elif command[0] == "-1": # Stop container
@@ -76,11 +117,17 @@ class DockerManager():
 
             time.sleep(0.3)
         
-    def stop_active_container(self):
-
+    def stop_active_container(self, do_pause = True):
+        print("self.active_container:", self.active_container)
         if self.active_container[0] is not None:
             try:
-                self.active_container[1].stop()
+                if do_pause:
+                    print(f"Pausing active docker.")
+                    self.active_container[1].pause()
+                else:    
+                    print(f"Stoping active docker.")
+                    self.active_container[1].stop()
+                    self.running_containers.pop(self.active_container[0])
 
                 w = echolib.MessageWriter()
                 w.writeString(self.active_container[0])
@@ -89,6 +136,7 @@ class DockerManager():
             except:
                 print("Error stopping docker container...")
 
+            print("setting self.active_container to None")
             self.active_container[0] = self.active_container[1] = None
 
     def __handle_container(self, tag):
